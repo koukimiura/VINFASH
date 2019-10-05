ffa6be931f26945f11c75ed43c4cd283c3d4e4e5
EC2 Default User <ec2-user@ip-172-31-5-60.us-east-2.compute.internal>
2019-10-05 11:14:19 +0000

    18commit

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-HEROKU_DEBUG_RAILS_RUNNER=1
+#HEROKU_DEBUG_RAILS_RUNNER=1
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
